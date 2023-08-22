4d80fb00b0f3e5ed7fbd87235dc1d840044501d7
GuyOnLeft <jmunson0711@gmail.com>
2023-08-22 15:49:10 -0700

    new commit for changed code

--- server.js
+++ server.js
@@ -34,4 +34,5 @@ const server = http.createServer((req, res) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server
+    console.log(`Server running on port ${PORT}`);
+});
